70494c54ecc8bea930cfe967988f8f41319f79e6
Marco <vogel.marco@mailbox.org>
2024-10-29 15:04:13 +0100

    default path

--- lib/arch/rvv/codec.c
+++ lib/arch/rvv/codec.c
@@ -19,17 +19,22 @@
 #include <riscv_vector.h>
 #endif // BASE64_USE_RVV
 
-// Only enable inline assembly on supported compilers.
-#if defined(__GNUC__) || defined(__clang__)
-#define BASE64_NEON64_USE_ASM
-#endif
-
 void base64_stream_encode_rvv BASE64_ENC_PARAMS
 {
+#ifdef BASE64_USE_RVV
+#include "../generic/enc_head.c"
+#include "../generic/enc_tail.c"
+#else
 	base64_enc_stub(state, src, srclen, out, outlen);
+#endif
 }
 
 int base64_stream_decode_rvv BASE64_DEC_PARAMS
 {
+#ifdef BASE64_USE_RVV
+#include "../generic/dec_head.c"
+#include "../generic/dec_tail.c"
+#else
 	return base64_dec_stub(state, src, srclen, out, outlen);
+#endif
 }
